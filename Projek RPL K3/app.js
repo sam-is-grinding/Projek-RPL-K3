d8df22543c8495ee48f7f2d2b4dd361ebec25336
sacemakesgame <mungkinchristian@apps.ipb.ac.id>
2025-06-02 22:16:09 +0700

    Initial commit

---
+++ Projek RPL K3/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
---
+++ README.md
@@ -0,0 +1,2 @@
+# Projek-RPL-K3
+
